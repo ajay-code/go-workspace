d203e7fe1ea7c674a716783858013f16b852172e
Ajay Singh <ajay10mar96@gmail.com>
2022-03-29 12:02:42 +0530

    making slack file upload bot

---
+++ slack-file-bot/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/slack-go/slack"
+)
+
+func main () {
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"sample.pdf"}
+
+	for i := 0; i < len(fileArr); i++{
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File: fileArr[i],
+		}
+
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL: %s, URLPrivate: %s\n", file.Name, file.URL, file.URLPrivate)
+	}
+ }
